2b5c581afb8ffb9426a6ac2134b0fa2c5530fb8f
SzetAdom <adamszeteli@gmail.com>
2021-05-19 21:43:48 +0200

    gameService done

--- src/java/Service/GameService.java
+++ src/java/Service/GameService.java
@@ -14,4 +14,40 @@ public class GameService {
         System.out.println("addGame");
         return GameRepo.addGame(game);
     }
+
+    public static Game getGame(int id) {
+        System.out.println("------------------------");
+        System.out.println("getGame");
+        return GameRepo.getGame(id);
+    }
+
+    public static Boolean getAllGame() {
+        System.out.println("------------------------");
+        System.out.println("getAllGame");
+        return GameRepo.getAllGame();
+    }
+
+    public static Boolean updateGame(Game game) {
+        System.out.println("------------------------");
+        System.out.println("updateGame");
+        return GameRepo.updateGame(game);
+    }
+
+    public static Boolean deleteGame(int id) {
+        System.out.println("------------------------");
+        System.out.println("deleteGame");
+        return GameRepo.deleteGame(id);
+    }
+
+    public static Boolean likeGame(int id) {
+        System.out.println("------------------------");
+        System.out.println("likeGame");
+        return GameRepo.likeGame(id);
+    }
+
+    public static Boolean dislikeGame(int id) {
+        System.out.println("------------------------");
+        System.out.println("dislikeGame");
+        return GameRepo.dislikeGame(id);
+    }
 }
