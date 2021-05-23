a852d3e17ca47070f756a2209a1667751e07b0fc
SzetAdom <adamszeteli@gmail.com>
2021-05-23 15:03:56 +0200

    Database updated: user, comment procedures

--- gamesite.sql
+++ gamesite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2021 at 04:04 PM
+-- Generation Time: May 23, 2021 at 03:03 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.1
 
@@ -25,6 +25,9 @@ DELIMITER $$
 --
 -- Procedures
 --
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addComment` (IN `in_text` VARCHAR(255) CHARSET utf8mb4, IN `in_likes` INT(11), IN `in_user_id` INT(11), IN `in_game_id` INT(11))  NO SQL
+INSERT INTO comment (comment.`text`, comment.`likes`, comment.`user_id`, comment.`game_id`) VALUES(in_text, in_likes, in_user_id, in_game_id)$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addCoupon` (IN `in_user_id` INT(11), IN `in_coupon_type_id` INT(11))  INSERT INTO coupon (coupon.user_id, coupon.coupon_type_id) VALUES(in_user_id, in_coupon_type_id)$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addCouponType` (IN `in_shop` VARCHAR(20), IN `in_value` INT(11), IN `in_purchaseable` TINYINT(1), IN `in_lasts` INT(11))  INSERT INTO coupon_type (coupon_type.shop, coupon_type.value, coupon_type.purchaseable, coupon_type.lasts) VALUES(in_shop, in_value, in_purchaseable, in_lasts)$$
@@ -35,8 +38,14 @@ INSERT INTO game (game.name, game.category, game.description, game.releasedate) 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `addStatistics` (IN `in_game_id` INT(11), IN `in_user_id` INT(11), IN `in_first_played` DATETIME, IN `in_last_played` DATETIME, IN `in_played_minutes` INT(11))  NO SQL
 INSERT INTO statistics (statistics.game_id, statistics.user_id, statistics.first_played, statistics.last_played,statistics.played_minutes) VALUES(in_game_id, in_user_id, in_first_played,in_last_played,in_played_minutes)$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addUser` (IN `in_username` VARCHAR(20) CHARSET utf8mb4, IN `in_password` TEXT CHARSET utf8mb4, IN `in_email` VARCHAR(100) CHARSET utf8mb4, IN `in_birth_date` DATE, IN `in_is_admin` BOOLEAN, IN `in_current_points` INT(11))  NO SQL
+INSERT INTO user(user.username, user.password, user.email, user.birth_date,user.isadmin,user.currentpoints) VALUES (in_username, SHA1(in_password), in_email, in_birth_date, in_is_admin, in_current_points)$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `dislikeGame` (IN `in_id` INT)  NO SQL
 UPDATE game SET game.likes = game.likes - 1 WHERE game.game_id = in_id$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllActiveComment` ()  NO SQL
+SELECT * FROM comment WHERE comment.isactive = 1$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllActiveCoupons` ()  SELECT * FROM coupon WHERE coupon.isactive = 1$$
 
@@ -47,6 +56,9 @@ SELECT * FROM game WHERE game.isactive =1$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllActiveStatistics` ()  NO SQL
 SELECT * FROM statistics WHERE statistics.isactive = 1$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllActiveUser` ()  NO SQL
+SELECT * FROM user WHERE user.isactive =1$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllPurchaseableCouponTypes` ()  SELECT * FROM coupon_type WHERE coupon_type.purchaseable = 1 AND coupon_type.isactive = 1$$
 
@@ -64,6 +76,9 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `getUsersCoupons` (IN `in_user_id` I
 CREATE DEFINER=`root`@`localhost` PROCEDURE `likeGame` (IN `in_id` INT(11))  NO SQL
 UPDATE game SET game.likes = game.likes+1 WHERE game.game_id = in_id$$
 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `logicalDeleteComment` (IN `in_id` INT(11))  NO SQL
+UPDATE comment SET comment.isactive = 0 WHERE comment.comment_id = in_id$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `logicalDeleteCoupon` (IN `in_id` INT(11))  UPDATE coupon SET coupon.isactive = 0 WHERE coupon.coupon_id = in_id$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `logicalDeleteCouponType` (IN `in_id` INT(11))  UPDATE coupon_type SET coupon_type.isactive = 0 WHERE coupon_type.coupon_type_id = in_id$$
@@ -73,6 +88,9 @@ UPDATE game SET game.isactive = 0 WHERE game.game_id = in_id$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `logicalDeleteStatistics` (IN `in_id` INT(11))  NO SQL
 UPDATE statistics SET statistics.isactive = 0 WHERE statistics.statistics_id = in_id$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `logicalDeleteUser` (IN `in_id` INT(11))  NO SQL
+UPDATE user SET user.isactive = 0 WHERE user.user_id = in_id$$
 
 DELIMITER ;
 
@@ -89,9 +107,15 @@ CREATE TABLE `comment` (
   `likes` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `game_id` int(11) NOT NULL,
-  `reply_to_id` int(11) NOT NULL,
   `isactive` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `text`, `time`, `likes`, `user_id`, `game_id`, `isactive`) VALUES
+(2, 'Nagyon jó game', '2021-05-23 13:01:41', 100, 3, 23, 1);
 
 -- --------------------------------------------------------
 
@@ -143,7 +167,7 @@ CREATE TABLE `game` (
 --
 
 INSERT INTO `game` (`game_id`, `name`, `description`, `category`, `likes`, `releasedate`, `isactive`) VALUES
-(1, 'a', 'dsad', 'dsadsa', 0, '2021-05-05', 1);
+(23, 'Game name', 'Boom boom', 'Shooter', 0, '2021-05-31', 1);
 
 -- --------------------------------------------------------
 
@@ -166,9 +190,7 @@ CREATE TABLE `statistics` (
 --
 
 INSERT INTO `statistics` (`statistics_id`, `game_id`, `user_id`, `first_played`, `last_played`, `played_minutes`, `isactive`) VALUES
-(4, 1, 1, '2021-01-01', '2021-01-01', 10, 1),
-(5, 1, 2, '2021-01-01', '2021-01-01', 10, 0),
-(12, 1, 2, '2021-02-01', '2021-02-04', 10, 0);
+(15, 23, 3, '2021-03-27', '2021-04-04', 10, 1);
 
 -- --------------------------------------------------------
 
@@ -192,8 +214,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `birth_date`, `isadmin`, `currentpoints`, `isactive`) VALUES
-(1, 'A', 'sdas', 'dsadas', '2021-05-05', 1, 10, 1),
-(2, 'dsa', 'dsa', 'dsa', '2021-05-03', 0, 20, 1);
+(3, 'Username', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'email@mail.com', '2001-04-05', 1, 0, 1);
 
 --
 -- Indexes for dumped tables
@@ -205,8 +226,7 @@ INSERT INTO `user` (`user_id`, `username`, `password`, `email`, `birth_date`, `i
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`comment_id`),
   ADD KEY `comment_user` (`user_id`),
-  ADD KEY `comment_game` (`game_id`),
-  ADD KEY `comment_reply_to` (`reply_to_id`);
+  ADD KEY `comment_game` (`game_id`);
 
 --
 -- Indexes for table `coupon`
@@ -250,7 +270,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `coupon`
@@ -268,19 +288,19 @@ ALTER TABLE `coupon_type`
 -- AUTO_INCREMENT for table `game`
 --
 ALTER TABLE `game`
-  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `statistics`
 --
 ALTER TABLE `statistics`
-  MODIFY `statistics_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `statistics_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -291,7 +311,6 @@ ALTER TABLE `user`
 --
 ALTER TABLE `comment`
   ADD CONSTRAINT `comment_game` FOREIGN KEY (`game_id`) REFERENCES `game` (`game_id`),
-  ADD CONSTRAINT `comment_reply_to` FOREIGN KEY (`reply_to_id`) REFERENCES `comment` (`comment_id`),
   ADD CONSTRAINT `comment_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
 
 --
